fae49026315c9dac938c48c671fd224160b37b8e
Nithin K <77234820+nithin1117@users.noreply.github.com>
2021-12-13 22:05:25 -0800

    Update Ftp.py

--- Ftp.py
+++ Ftp.py
@@ -4,7 +4,6 @@ import shutil
 import argparse
 import os
 
-
 ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--date", help="name of the user")
 args = vars(ap.parse_args())
@@ -20,7 +19,7 @@ def AutoDate():
 
 
 if dmy == None:
-    dmy= AutoDate()
+    dmy = AutoDate()
 
 else:
     import datetime
@@ -29,7 +28,7 @@ else:
 
     while True:
         day = dmy
-        try :
+        try:
             day = datetime.datetime.strptime(day, "%d/%m/%Y")
             break
         except ValueError:
@@ -38,8 +37,6 @@ else:
 
     mon = calendar.month_abbr[int(day.month)]
     dmy = (f"{day.day}" + mon + f"{day.year}")
-
-
 
 if dmy[1].isalpha():
     dmy = dmy.zfill(9)
@@ -55,66 +52,60 @@ def ftpfile(dmy):
     ftp.cwd(path)
 
     try:
-        
+
         key = 'FTPFILES'
-        dir1 = os.getenv(key) + "/"     
-      
-        #dir1 = "/home/nithin/Documents/sample/new/check/"
+        dir1 = os.getenv(key) + "/"
 
-        fName1 = "contract.gz_" + dmy
-        fName2 = "nnf_participant.gz_" + dmy
-        fName3 = "nnf_security.gz_" + dmy
-        fName4 = "participant.gz_" + dmy
-        fName5 = "security.gz"
-        fName6 = "spd_contract.gz"
-
-        pdir1 = dir1 + fName1
-        pdir2 = dir1 + fName2
-        pdir3 = dir1 + fName3
-        pdir4 = dir1 + fName4
-        pdir5 = dir1 + fName5
-        pdir6 = dir1 + fName6
-
-        ftp.retrbinary("RETR " + fName1, open(pdir1, 'wb').write)
-        ftp.retrbinary("RETR " + fName2, open(pdir2, 'wb').write)
-        ftp.retrbinary("RETR " + fName3, open(pdir3, 'wb').write)
-        ftp.retrbinary("RETR " + fName4, open(pdir4, 'wb').write)
-        ftp.retrbinary("RETR " + fName5, open(pdir5, 'wb').write)
-        ftp.retrbinary("RETR " + fName6, open(pdir6, 'wb').write)
-
-        ftp.close()
-        print("Downloaded")
+        # dir1 = "/home/nithin/Documents/sample/new/check/"
         try:
-            with open(pdir1, 'rb') as f_in:
-                with gzip.open(pdir1[:-13] + dmy + '.txt', 'wb') as f_out:
+            fName1 = "contract.gz_" + dmy
+            fName2 = "nnf_participant.gz_" + dmy
+            fName3 = "nnf_security.gz_" + dmy
+            fName4 = "participant.gz_" + dmy
+            fName5 = "security.gz"
+            fName6 = "spd_contract.gz"
+
+            ftp.retrbinary("RETR " + fName1, open(dir1 + fName1, 'wb').write)
+            ftp.retrbinary("RETR " + fName2, open(dir1 + fName2, 'wb').write)
+            ftp.retrbinary("RETR " + fName3, open(dir1 + fName3, 'wb').write)
+            ftp.retrbinary("RETR " + fName4, open(dir1 + fName4, 'wb').write)
+            ftp.retrbinary("RETR " + fName5, open(dir1 + fName5, 'wb').write)
+            ftp.retrbinary("RETR " + fName6, open(dir1 + fName6, 'wb').write)
+
+            ftp.close()
+            with open(dir1 + fName1, 'rb') as f_in:
+                with gzip.open(dir1 + fName1[:-13] + dmy + '.txt', 'wb') as f_out:
                     shutil.copyfileobj(f_in, f_out)
 
-            with open(pdir2, 'rb') as f_in:
-                with gzip.open(pdir2[:-13] + dmy + '.txt', 'wb') as f_out:
+            with open(dir1 + fName2, 'rb') as f_in:
+                with gzip.open(dir1 + fName2[:-13] + dmy + '.txt', 'wb') as f_out:
                     shutil.copyfileobj(f_in, f_out)
 
-            with open(pdir3, 'rb') as f_in:
-                with gzip.open(pdir3[:-13] + dmy + '.txt', 'wb') as f_out:
+            with open(dir1 + fName3, 'rb') as f_in:
+                with gzip.open(dir1 + fName3[:-13] + dmy + '.txt', 'wb') as f_out:
                     shutil.copyfileobj(f_in, f_out)
 
-            with open(pdir4, 'rb') as f_in:
-                with gzip.open(pdir4[:-13] + dmy + '.txt', 'wb') as f_out:
+            with open(dir1 + fName4, 'rb') as f_in:
+                with gzip.open(dir1 + fName4[:-13] + dmy + '.txt', 'wb') as f_out:
                     shutil.copyfileobj(f_in, f_out)
 
-            with open(pdir5, 'rb') as f_in:
-                with gzip.open(pdir5[:-4] + '.txt', 'wb') as f_out:
+            with open(dir1 + fName5, 'rb') as f_in:
+                with gzip.open(dir1 + fName5[:-4] + '.txt', 'wb') as f_out:
                     shutil.copyfileobj(f_in, f_out)
 
-            with open(pdir6, 'rb') as f_in:
-                with gzip.open(pdir6[:-4] + '.txt', 'wb') as f_out:
+            with open(dir1 + fName6, 'rb') as f_in:
+                with gzip.open(dir1 + fName6[:-4] + '.txt', 'wb') as f_out:
                     shutil.copyfileobj(f_in, f_out)
 
-            print("Extracted!!!...")
+            print("Downloaded and Extracted!!!...")
+
         except:
-            print("zip file path can't found")
-
-    except :
-        print("File not Found in FTP")
+            print("Error: File not found in FTP")
+            return "error"
+    except:
+        print("Error: path location had not prefered")
+        print("run 'sudo gedit /etc/environment' and set 'FTPFILES = /*desired directory*/'")
+        sys.exit()
 
 
 ftpfile(dmy)
