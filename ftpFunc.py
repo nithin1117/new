e4794707789a6f9a35b8ce06bf17017f93fd2d88
Nithin K <77234820+nithin1117@users.noreply.github.com>
2021-12-13 21:09:52 -0800

    Update ftpFunc.py

--- ftpFunc.py
+++ ftpFunc.py
@@ -20,7 +20,6 @@ def AutoDate():
 
 if dmy == None:
     dmy = AutoDate()
-
 else:
     import datetime
     import calendar
@@ -32,41 +31,44 @@ else:
             day = datetime.datetime.strptime(day, "%d/%m/%Y")
             break
         except ValueError:
-            print('Error: Invalid Date Format, Try DD/MM/YYY')
+            print('Error: Invalid Date Format, Try DD/MM/YYYY')
             sys.exit()
 
     mon = calendar.month_abbr[int(day.month)]
     dmy = (f"{day.day}" + mon + f"{day.year}")
+
 
 if dmy[1].isalpha():
     dmy = dmy.zfill(9)
 else:
     pass
 
-    
-def ftpfile(dmy, fName): 
+
+def ftpfile(dmy, fName):
     ftp = FTP('ftp.connect2nse.com')
     ftp.login('FTPGUEST', 'FTPGUEST')
     path = '/Common/NTNEAT'
     ftp.cwd(path)
-
-
     try:
         key = 'FTPFILES'
         dir = os.getenv(key) + "/"
         # dir = "/home/nithin/Documents/sample/new/check/"
+        try:
+            pathDir = dir + fName
+            ftp.retrbinary("RETR " + fName, open(pathDir, 'wb').write)
+            ftp.close()
 
-        pathDir = dir + fName
-        ftp.retrbinary("RETR " + fName, open(pathDir, 'wb').write)
-        ftp.close()
-        
-        with open(pathDir, 'rb') as f_in:
-            with gzip.open(pathDir + '.txt', 'wb') as f_out:
-                shutil.copyfileobj(f_in, f_out)
+            with open(pathDir, 'rb') as f_in:
+                with gzip.open(pathDir + '.txt', 'wb') as f_out:
+                    shutil.copyfileobj(f_in, f_out)
 
+        except:
+            print("Error: File not found for particular date in FTP")
+            return "error"
     except:
-        print("File not found in FTP")
-        return "error"
+        print("Error: path location had not prefered")
+        print("run 'sudo gedit /etc/environment' and set 'FTPFILES = *desired directory*'")
+        sys.exit() 
 
 
 check = ftpfile(dmy, "contract.gz_" + dmy)
@@ -78,4 +80,3 @@ if check != "error":
     ftpfile(dmy, "security.gz")
     ftpfile(dmy, "spd_contract.gz")
     print("Downloaded and extracted!")
-    
