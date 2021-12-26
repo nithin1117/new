dbb5eed16e67e130b15342c11136e594c0dd4aa1
Nithin K <77234820+nithin1117@users.noreply.github.com>
2021-12-26 14:20:09 +0530

    Update ftpFunc.py

--- ftpFunc.py
+++ ftpFunc.py
@@ -3,9 +3,10 @@ import gzip
 import shutil
 import argparse
 import os
+import sys
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-d", "--date", help="name of the user")
+ap.add_argument("-d", "--date")
 args = vars(ap.parse_args())
 dmy = args["date"]
 
@@ -23,20 +24,17 @@ if dmy == None:
 else:
     import datetime
     import calendar
-    import sys
 
-    while True:
-        day = dmy
-        try:
-            day = datetime.datetime.strptime(day, "%d/%m/%Y")
-            break
-        except ValueError:
-            print('Error: Invalid Date Format, Try DD/MM/YYYY')
-            sys.exit()
+    day = dmy
+    try:
+        day = datetime.datetime.strptime(day, "%d/%m/%Y")
+
+    except ValueError:
+        print('Error: Invalid Date Format, Try -d DD/MM/YYYY')
+        sys.exit()
 
     mon = calendar.month_abbr[int(day.month)]
     dmy = (f"{day.day}" + mon + f"{day.year}")
-
 
 if dmy[1].isalpha():
     dmy = dmy.zfill(9)
@@ -44,7 +42,7 @@ else:
     pass
 
 
-def ftpfile(dmy, fName):
+def ftpfile(dmy):
     ftp = FTP('ftp.connect2nse.com')
     ftp.login('FTPGUEST', 'FTPGUEST')
     path = '/Common/NTNEAT'
@@ -52,31 +50,43 @@ def ftpfile(dmy, fName):
     try:
         key = 'FTPFILES'
         dir = os.getenv(key) + "/"
-        # dir = "/home/nithin/Documents/sample/new/check/"
+        # dir = "/home/nithin/datatCollect/trimData/test/src/script/folder/"
         try:
-            pathDir = dir + fName
-            ftp.retrbinary("RETR " + fName, open(pathDir, 'wb').write)
-            ftp.close()
+            fName = [
+                "contract.gz_" + dmy,
+                "nnf_participant.gz_" + dmy,
+                "nnf_security.gz_" + dmy,
+                "participant.gz_" + dmy,
+                "security.gz",
+                "spd_contract.gz",
+            ]
+            for i in range(4):
+                pathDir = dir + fName[i]
+                ftp.retrbinary("RETR " + fName[i], open(pathDir, 'wb').write)
 
-            with open(pathDir, 'rb') as f_in:
-                with gzip.open(pathDir + '.txt', 'wb') as f_out:
-                    shutil.copyfileobj(f_in, f_out)
+                with open(pathDir, 'rb') as f_in:
+                    with gzip.open(pathDir[:-13] + dmy + '.txt', 'wb') as f_out:
+                        shutil.copyfileobj(f_in, f_out)
+
+            for i in range(4, 6):
+                pathDir = dir + fName[i]
+                ftp.retrbinary("RETR " + fName[i], open(pathDir, 'wb').write)
+
+                with open(pathDir, 'rb') as f_iin:
+                    with gzip.open(pathDir[:-3] + '.txt', 'wb') as f_out:
+                        shutil.copyfileobj(f_iin, f_out)
+            ftp.close()
 
         except:
             print("Error: File not found for particular date in FTP")
-            return "error"
+            return "null"
+
     except:
         print("Error: path location had not prefered")
         print("run 'sudo gedit /etc/environment' and set 'FTPFILES = *desired directory*'")
-        sys.exit() 
+        sys.exit()
 
 
-check = ftpfile(dmy, "contract.gz_" + dmy)
-
-if check != "error":
-    ftpfile(dmy, "nnf_participant.gz_" + dmy)
-    ftpfile(dmy, "nnf_security.gz_" + dmy)
-    ftpfile(dmy, "participant.gz_" + dmy)
-    ftpfile(dmy, "security.gz")
-    ftpfile(dmy, "spd_contract.gz")
-    print("Downloaded and extracted!")
+check = ftpfile(dmy)
+if check != "null":
+    print("Downloaded and extracted!...")
